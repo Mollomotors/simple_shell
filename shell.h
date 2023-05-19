5d8c55cb5cbe00145a48e5d34963df9a77917303
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-19 12:58:52 +0100

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+#define END_OF_FILE -2
+#define EXIT -3
+
+
+#endif /* _SHELL_H_ */
