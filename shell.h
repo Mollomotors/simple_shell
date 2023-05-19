70d90f4eafdc90ec3b575be94790afc00777c61c
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-19 13:11:22 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -13,5 +13,25 @@
 #define END_OF_FILE -2
 #define EXIT -3
 
+// functions from sh_atoi
+int interactive(info_t *info);
+int _isalpha(int c);
+int is_delim(char c, char *delim);
+int _atoi(char *s);
 
-#endif /* _SHELL_H_ */
+// functions from sh_builtin1
+int _myexit(info_t *info);
+int _mycd(info_t *info);
+int _myhelp(info_t *info);
+int _myhistory(info_t *info);
+
+// functions from sh_builtin2
+int unset_alias(info_t *info, char *str);
+int set_alias(info_t *info, char *str);
+int print_alias(list_t *node);
+int _myalias(info_t *info);
+
+// functions from sh_memory
+int bfree(void **ptr)
+
+#endif
