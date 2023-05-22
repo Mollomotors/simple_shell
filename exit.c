a00a1e4017cee288f1445704113da1b972c052b6
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-22 09:31:59 +0100

    Create exit.c

---
+++ exit.c
@@ -0,0 +1,65 @@
+#include "shell.h"
+
+/**
+ *Simple shell 0.4 +
+ *handle arguments for the built-in exit
+ *Usage: exit status, where status is an integer used to exit the shell
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
+ */
+ 
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, k;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		k = i;
+		while (k < n)
+		{
+			dest[k] = '\0';
+			k++;
+		}
+	}
+	return (s);
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, k;
+	char *s = dest;
+
+	i = 0;
+	k = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[k] != '\0' && k < n)
+	{
+		dest[i] = src[k];
+		i++;
+		k++;
+	}
+	if (k < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
