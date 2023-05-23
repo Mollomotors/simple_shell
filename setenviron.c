bcad0bd90f5d96c85c51f987abd09badea86abb4
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-23 16:02:50 +0100

    Update setenviron.c

--- setenviron.c
+++ setenviron.c
@@ -1,89 +1,82 @@
 #include "shell.h"
+
 /**
-
-**Implement the setenv and unsetenv builtin commands
-
-*setenv
-*Initialize a new environment variable, or modify an existing one
-*Command syntax: setenv VARIABLE VALUE
-*Should print something on stderr on failure
-*unsetenv
-*Remove an environment variable
-*Command syntax: unsetenv VARIABLE
-*Should print something on stderr on failure
-*/
-
-int _myenv(info_t *info)
+ * get_environ - returns the string array copy of our environ
+ */
+char **get_environ(info_t *info)
 {
-	print_list_str(info->env);
-	return (0);
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
 }
 
 /**
- * _getenv - gets the value of an environ variable
+ * _unsetenv - Remove an environment variable
+ * @var: the string env var property
  */
-char *_getenv(info_t *info, const char *name)
+int _unsetenv(info_t *info, char *var)
 {
 	list_t *node = info->env;
+	size_t i = 0;
 	char *p;
+
+	if (!node || !var)
+		return (0);
 
 	while (node)
 	{
-		p = starts_with(node->str, name);
-		if (p && *p)
-			return (p);
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * _setenv - Initialize a new environment variable,
+ *  Return: Always 0
+ */
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
 		node = node->next;
 	}
-	return (NULL);
-}
-
-/**
- * _mysetenv - Initialize a new environment variable,
- * or modify an existing one
- *  Return: Always 0
- */
-int _mysetenv(info_t *info)
-{
-	if (info->argc != 3)
-	{
-		_eputs("Incorrect number of arguements\n");
-		return (1);
-	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
-		return (0);
-	return (1);
-}
-
-/**
- * _myunsetenv - Remove an environment variable
- *  Return: Always 0
- */
-int _myunsetenv(info_t *info)
-{
-	int i;
-
-	if (info->argc == 1)
-	{
-		_eputs("Too few arguements.\n");
-		return (1);
-	}
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
-
-	return (0);
-}
-
-/**
- * populate_env_list - populates env linked list
- * Return: Always 0
- */
-int populate_env_list(info_t *info)
-{
-	list_t *node = NULL;
-	size_t i;
-
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
 	return (0);
 }
