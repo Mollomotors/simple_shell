762f63a9e1abad2b031de83a69ce8b3027e0c57f
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 14:38:43 +0100

    Update comment_error.c

--- comment_error.c
+++ comment_error.c
@@ -1,5 +1,5 @@
 #include "shell.h"
-
+/* remove_comments function */
 void remove_comments(char *buf)
 {
 	int i;
@@ -10,4 +10,32 @@ void remove_comments(char *buf)
 			buf[i] = '\0';
 			break;
 		}
+}
+
+/* eputs function */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/* from my history file */
+int build_history_list(info_t *info, char *buf, int linecount)
+{
+	list_t *node = NULL;
+
+	if (info->history)
+		node = info->history;
+	add_node_end(&node, buf, linecount);
+
+	if (!info->history)
+		info->history = node;
+	return (0);
 }
