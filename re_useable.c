60c59f61794a89a41bc414dc2fe2e629a745aa0f
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 15:20:27 +0100

    Update re_useable.c

--- re_useable.c
+++ re_useable.c
@@ -114,3 +114,15 @@ int _eputchar(char c)
 		buf[i++] = c;
 	return (1);
 }
+
+/* function from environ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
+}
