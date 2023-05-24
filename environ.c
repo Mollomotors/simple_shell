8b2bd62bfa026e9c8221742df5c2f44608ed2a87
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 12:50:25 +0100

    Update environ.c

--- environ.c
+++ environ.c
@@ -1,76 +1,70 @@
 #include "shell.h"
 
-int _myenv(info_t *info)
+char **_copyenv(void);
+void free_env(void);
+char **_getenv(const char *var);
+
+/**
+ * _copyenv - Creates a copy of the environment.
+ * Return: If an error occurs - NULL.
+ */
+char **_copyenv(void)
 {
-	print_list_str(info->env);
-	return (0);
+	char **new_environ;
+	size_t size;
+	int index;
+
+	for (size = 0; environ[size]; size++)
+		;
+
+	new_environ = malloc(sizeof(char *) * (size + 1));
+	if (!new_environ)
+		return (NULL);
+
+	for (index = 0; environ[index]; index++)
+	{
+		new_environ[index] = malloc(_strlen(environ[index]) + 1);
+
+		if (!new_environ[index])
+		{
+			for (index--; index >= 0; index--)
+				free(new_environ[index]);
+			free(new_environ);
+			return (NULL);
+		}
+		_strcpy(new_environ[index], environ[index]);
+	}
+	new_environ[index] = NULL;
+
+	return (new_environ);
 }
 
 /**
- * _getenv - gets the value of an environ variable
- * Return: the value
+ * free_env - Frees the the environment copy.
  */
-char *_getenv(info_t *info, const char *name)
+void free_env(void)
 {
-	list_t *node = info->env;
-	char *p;
+	int index;
 
-	while (node)
+	for (index = 0; environ[index]; index++)
+		free(environ[index]);
+	free(environ);
+}
+
+/**
+ * _getenv - Gets an environmental variable from the PATH 
+ * Return: If the environmental variable does not exist - NULL.
+ */
+char **_getenv(const char *var)
+{
+	int index, len;
+
+	len = _strlen(var);
+	for (index = 0; environ[index]; index++)
 	{
-		p = starts_with(node->str, name);
-		if (p && *p)
-			return (p);
-		node = node->next;
+		if (_strncmp(var, environ[index], len) == 0)
+			return (&environ[index]);
 	}
+
 	return (NULL);
-}
-
-/**
- * _mysetenv - Initialize a new environment variable,
- *  Return: Always 0
- */
-int _mysetenv(info_t *info)
-{
-	if (info->argc != 3)
-	{
-		_eputs("Incorrect number of arguements\n");
-		return (1);
-	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
-		return (0);
-	return (1);
-}
-
-/**
- * _myunsetenv - Remove an environment variable
- *  Return: Always 0
- */
-int _myunsetenv(info_t *info)
-{
-	int i;
-
-	if (info->argc == 1)
-	{
-		_eputs("Too few arguements.\n");
-		return (1);
-	}
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
-
-	return (0);
-}
-
-/**
- * populate_env_list - populates env linked list
- * Return: Always 0
- */
-int populate_env_list(info_t *info)
-{
-	list_t *node = NULL;
-	size_t i;
-
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
-	return (0);
 }
