297288343d5b0fcd8a7c5e79d2c48ec5434925ff
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 12:05:45 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -87,38 +87,38 @@ typedef struct builtin
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 	0, 0, 0}
 
-// functions from sh_atoi
+/*functions from sh_atoi */
 int interactive(info_t *info);
 int _isalpha(int c);
 int is_delim(char c, char *delim);
 int _atoi(char *s);
 
-// functions from sh_builtin1
+/* functions from sh_builtin1 */
 int _myexit(info_t *info);
 int _mycd(info_t *info);
 int _myhelp(info_t *info);
 int _myhistory(info_t *info);
 
-// functions from sh_builtin2
+/* functions from sh_builtin2 */
 int unset_alias(info_t *info, char *str);
 int set_alias(info_t *info, char *str);
 int print_alias(list_t *node);
 int _myalias(info_t *info);
 
-// functions from sh_memory
+/* functions from sh_memory */
 int bfree(void **ptr);
   
-// fnction from getline (exercise 6)
+/* fnction from getline (exercise 6) */
 ssize_t get_input(info_t *);
 int _getline(info_t *info, char **ptr, size_t *length);
 void sigintHandler(__attribute__((unused))int sig_num);
 
-//function from excerise 8
+/* function from excerise 8 */
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strchr(char *s, char c);
 
-// functions from vars
+/* functions from vars */
 int is_chain(info_t *, char *, size_t *);
 void check_chain(info_t *, char *, size_t *, size_t, size_t);
 int replace_alias(info_t *);
