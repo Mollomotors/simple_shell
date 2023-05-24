238ec520176d057b9762d4ae40c8127f2c78f2bb
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 12:14:06 +0100

    Update getLine.c

--- getLine.c
+++ getLine.c
@@ -2,14 +2,6 @@
 
 /**
 *Simple shell 0.1 +
-
-*Write your own getline function
-Use a buffer to read many chars at once and call the least possible the read system call
-You will need to use static variables
-You are not allowed to use getline
-You don’t have to:
-
-be able to move the cursor
  */
 
 ssize_t input_buf(info_t *info, char **buf, size_t *len)
