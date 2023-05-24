0e4ff439b58ea10db2cea2df2797ce1d3be17fa2
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 15:27:08 +0100

    Update re_useable.c

--- re_useable.c
+++ re_useable.c
@@ -170,3 +170,40 @@ int read_history(info_t *info)
 	renumber_history(info);
 	return (info->histcount);
 }
+
+/* function from loop */
+int hsh(info_t *info, char **av)
+{
+	ssize_t r = 0;
+	int builtin_ret = 0;
+
+	while (r != -1 && builtin_ret != -2)
+	{
+		clear_info(info);
+		if (interactive(info))
+			_puts("$ ");
+		_eputchar(BUF_FLUSH);
+		r = get_input(info);
+		if (r != -1)
+		{
+			set_info(info, av);
+			builtin_ret = find_builtin(info);
+			if (builtin_ret == -1)
+				find_cmd(info);
+		}
+		else if (interactive(info))
+			_putchar('\n');
+		free_info(info, 0);
+	}
+	write_history(info);
+	free_info(info, 1);
+	if (!interactive(info) && info->status)
+		exit(info->status);
+	if (builtin_ret == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (builtin_ret);
+}
