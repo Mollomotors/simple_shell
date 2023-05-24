0488abecc89c618475bddb0242d33b69b8c6c749
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 12:37:45 +0100

    Update environ.c

--- environ.c
+++ environ.c
@@ -1,14 +1,15 @@
 #include "shell.h"
 
- /* _myenv - prints the current environment */
 int _myenv(info_t *info)
 {
 	print_list_str(info->env);
 	return (0);
 }
 
- /*  _getenv - gets the value of an environ variable */
-
+/**
+ * _getenv - gets the value of an environ variable
+ * Return: the value
+ */
 char *_getenv(info_t *info, const char *name)
 {
 	list_t *node = info->env;
@@ -26,9 +27,6 @@ char *_getenv(info_t *info, const char *name)
 
 /**
  * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
  *  Return: Always 0
  */
 int _mysetenv(info_t *info)
@@ -45,8 +43,6 @@ int _mysetenv(info_t *info)
 
 /**
  * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
  *  Return: Always 0
  */
 int _myunsetenv(info_t *info)
@@ -66,8 +62,6 @@ int _myunsetenv(info_t *info)
 
 /**
  * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
  * Return: Always 0
  */
 int populate_env_list(info_t *info)
