e362b1a72540909aaff7592d06f747c60c3f72f0
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 16:16:23 +0100

    Update re_useable.c

--- re_useable.c
+++ re_useable.c
@@ -385,3 +385,27 @@ int write_history(info_t *info)
 	close(fd);
 	return (1);
 }
+
+/* function from getinfo */
+void clear_info(info_t *info)
+{
+	info->arg = NULL;
+	info->argv = NULL;
+	info->path = NULL;
+	info->argc = 0;
+}
+/* function from environ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
