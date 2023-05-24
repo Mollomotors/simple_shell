ab66f04f4a2506cd08a99ea74915c18d7602ed51
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 13:24:50 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -135,6 +135,10 @@ size_t print_list(const list_t *);
 list_t *node_starts_with(list_t *, char *, char);
 ssize_t get_node_index(list_t *, list_t *);
 
-
+/*  fnctions from string.c */
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
 
 #endif
