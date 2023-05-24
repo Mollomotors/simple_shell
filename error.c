8be6b404d2c7e3e18ceab7d39dc1cb7606af2b99
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-25 00:47:50 +0100

    Update error.c

--- error.c
+++ error.c
@@ -1,25 +1,6 @@
 #include "shell.h"
 
 /**
- *_eputs - prints an input string
- * @str: the string to be printed
- *
- * Return: Nothing
- */
-void _eputs(char *str)
-{
-	int i = 0;
-
-	if (!str)
-		return;
-	while (str[i] != '\0')
-	{
-		_eputchar(str[i]);
-		i++;
-	}
-}
-
-/**
  * _putfd - writes the character c to given fd
  * @c: The character to print
  * @fd: The filedescriptor to write to
