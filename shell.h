6b6e530e1e10c19b7cec9077692d7559a3527664
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 12:27:00 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -67,25 +67,16 @@ typedef struct passinfo
 	int histcount;
 } info_t;
 
+
 #define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 	0, 0, 0}
 
-/**
- *struct builtin - contains a builtin string and related function
- *@type: the builtin command flag
- *@func: the function
- */
 typedef struct builtin
 {
 	char *type;
 	int (*func)(info_t *);
 } builtin_table;
-
-
-#define INFO_INIT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-	0, 0, 0}
 
 /*functions from sh_atoi */
 int interactive(info_t *info);
