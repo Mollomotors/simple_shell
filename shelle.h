4155820a23d0387b3e4187a413ebaee36cf1fd20
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 09:34:01 +0100

    Update and rename shell.h to shelle.h

--- shelle.h
+++ shelle.h
@@ -1,14 +1,16 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
-#include <fcntl.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
 #include <errno.h>
-#include <stdio.h>
 
 #define END_OF_FILE -2
 #define EXIT -3
