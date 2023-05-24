96a24bf11ca1e3939b95b1cbab0ced60e8a95d95
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 15:07:48 +0100

    Update re_useable.c

--- re_useable.c
+++ re_useable.c
@@ -69,3 +69,32 @@ void _puts(char *str)
 		i++;
 	}
 }
+
+/* function from error1 */
+char *convert_number(long int num, int base, int flags)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
