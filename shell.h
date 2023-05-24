c561b0ab0d2057d87b688cc99f29b5c418acc037
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 13:00:12 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -116,4 +116,14 @@ int replace_alias(info_t *);
 int replace_vars(info_t *);
 int replace_string(char **, char *);
 
+int _strlen(const char *s);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+char *_strcpy(char *dest, const char *src);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
+
+
 #endif
