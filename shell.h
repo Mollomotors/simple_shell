c1f3fc50ba1328d067baaf9684754c042e9a06fe
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 22:13:27 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -10,19 +10,15 @@
 #include <limits.h>
 #include <fcntl.h>
 #include <errno.h>
-/* for read/write buffers */
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
-/* for command chaining */
 #define CMD_NORM	0
 #define CMD_OR		1
 #define CMD_AND		2
 #define CMD_CHAIN	3
-/* for convert_number() */
 #define CONVERT_LOWERCASE	1
 #define CONVERT_UNSIGNED	2
-/* 1 if using system getline() */
 #define USE_GETLINE 0
 #define USE_STRTOK 0
 #define HIST_FILE	".simple_shell_history"
