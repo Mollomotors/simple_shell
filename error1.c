872acf508b2676abf5a584ffee20695b693917e2
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-25 00:46:50 +0100

    Update error1.c

--- error1.c
+++ error1.c
@@ -84,21 +84,3 @@ int print_d(int input, int fd)
 
 	return (count);
 }
-
-/**
- * remove_comments - function replaces first instance of '#' with '\0'
- * @buf: address of the string to modify
- *
- * Return: Always 0;
- */
-void remove_comments(char *buf)
-{
-	int i;
-
-	for (i = 0; buf[i] != '\0'; i++)
-		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
-		{
-			buf[i] = '\0';
-			break;
-		}
-}
