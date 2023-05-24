7cb094b14dd5652bf25878fb52ad4b098a7e63b9
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 13:14:32 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -121,5 +121,20 @@ int _strspn(char *s, char *accept);
 int _strcmp(char *s1, char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
 
+/*  functions fron lists */
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
+size_t print_list_str(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
+
+/* functions from lists1 */
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
+list_t *node_starts_with(list_t *, char *, char);
+ssize_t get_node_index(list_t *, list_t *);
+
+
 
 #endif
