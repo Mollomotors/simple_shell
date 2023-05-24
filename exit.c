ab389565fc2d44a8bcbd98ab6a3720426fb67c37
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 13:10:05 +0100

    Update exit.c

--- exit.c
+++ exit.c
@@ -34,7 +34,7 @@ char *_strncpy(char *dest, char *src, int n)
 	return (s);
 }
 
-char *_strncat(char *dest, const char *src, size_t n);
+char *_strncat(char *dest, const char *src, size_t n)
 {
 	int i, k;
 	char *s = dest;
