fa6dbef27d3cbad40b15983399fb6a12245a7da5
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-24 21:50:14 +0100

    Update environ.c

--- environ.c
+++ environ.c
@@ -5,21 +5,26 @@ void free_env(void);
 char **_getenv(const char *var);
 
 /**
- * _getenv
- * return (new_environ)
+ * _copyenv - Creates a copy of the environment.
+ * Return: If an error occurs - NULL.
  */
 char **_copyenv(void)
 {
 	char **new_environ;
 	size_t size;
 	int index;
-	for (size = 0; environ[size]; size++);
+
+	for (size = 0; environ[size]; size++)
+		;
+
 	new_environ = malloc(sizeof(char *) * (size + 1));
 	if (!new_environ)
 		return (NULL);
+
 	for (index = 0; environ[index]; index++)
 	{
 		new_environ[index] = malloc(_strlen(environ[index]) + 1);
+
 		if (!new_environ[index])
 		{
 			for (index--; index >= 0; index--)
@@ -30,28 +35,37 @@ char **_copyenv(void)
 		_strcpy(new_environ[index], environ[index]);
 	}
 	new_environ[index] = NULL;
+
 	return (new_environ);
 }
+
+/**
+ * free_env - Frees the the environment copy.
+ * return 0
+ */
 void free_env(void)
 {
 	int index;
+
 	for (index = 0; environ[index]; index++)
 		free(environ[index]);
 	free(environ);
 }
 
 /**
- * _getenv - Gets an environmental variable from the PATH 
- * Return: If the environmental variable does not exist - NULL.
+ * _getenv - Gets an environmental variable from the PATH.
+ * @var: The name of the environmental variable to get.
  */
 char **_getenv(const char *var)
 {
 	int index, len;
+
 	len = _strlen(var);
 	for (index = 0; environ[index]; index++)
 	{
 		if (_strncmp(var, environ[index], len) == 0)
 			return (&environ[index]);
 	}
+
 	return (NULL);
 }
