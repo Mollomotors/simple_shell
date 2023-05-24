ac0820e3dc6ee53ab9cfbae4ab6852659b6f37fa
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-25 00:07:14 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -183,4 +183,9 @@ int _myenv(info_t *);
 int _mysetenv(info_t *);
 int _myunsetenv(info_t *);
 int populate_env_list(info_t *);
+
+/* getenv.c */
+char **get_environ(info_t *);
+int _unsetenv(info_t *, char *);
+int _setenv(info_t *, char *, char *);
 #endif
