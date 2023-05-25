1096605476a250dd0aa14846de56df6877148b7b
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-25 10:23:54 +0100

    Create exit.c

---
+++ exit.c
@@ -0,0 +1,60 @@
+#include “shell.h”
+/**
+ **_strncpy - copies a string
+ *Return: the concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+	i = 0;
+	while (src[i] != ‘\0’ && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = ‘\0’;
+			j++;
+		}
+	}
+	return (s);
+}
+/**
+ **_strncat - concatenates two strings
+ *Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+	i = 0;
+	j = 0;
+	while (dest[i] != ‘\0’)
+		i++;
+	while (src[j] != ‘\0’ && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = ‘\0’;
+	return (s);
+}
+/**
+ **_strchr - locates a character in a string
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != ‘\0’);
+	return (NULL);
+}
