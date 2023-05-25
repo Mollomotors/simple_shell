47d7c477cfdbeed03efe6c40ba31edae9311cec0
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-25 14:20:34 +0100

    Update getline.c

--- getline.c
+++ getline.c
@@ -2,8 +2,9 @@
 
 /**
  * input_buf - buffers chained commands
+ * @info: parameter struct definition
+ * @buf: address of buffer
  * @len: address of len var
- *
  * Return: bytes read
  */
 ssize_t input_buf(info_t *info, char **buf, size_t *len)
@@ -44,6 +45,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 
 /**
  * get_input - gets a line minus the newline
+ * @info: parameter struct definition
  * Return: bytes read
  */
 ssize_t get_input(info_t *info)
@@ -87,8 +89,9 @@ ssize_t get_input(info_t *info)
 
 /**
  * read_buf - reads a buffer
+ * @info: parameter struct
+ * @buf: buffer
  * @i: size
- *
  * Return: r
  */
 ssize_t read_buf(info_t *info, char *buf, size_t *i)
@@ -106,6 +109,8 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
 /**
  * _getline - gets the next line of input from STDIN
  * @info: parameter struct
+ * @ptr: address of pointer to buffer, preallocated or NULL
+ * @length: size of preallocated ptr buffer if not NULL
  * Return: s
  */
 int _getline(info_t *info, char **ptr, size_t *length)
@@ -150,6 +155,7 @@ int _getline(info_t *info, char **ptr, size_t *length)
 /**
  * sigintHandler - blocks ctrl-C
  * @sig_num: the signal number
+ * Return: void
  */
 void sigintHandler(__attribute__((unused))int sig_num)
 {
