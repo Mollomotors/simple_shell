c8000898a0680e0d75028bb7bde846d3a6524813
APEH <114231115+Mollomotors@users.noreply.github.com>
2023-05-25 12:28:30 +0100

    Update exit.c

--- exit.c
+++ exit.c
@@ -1,16 +1,17 @@
-#include “shell.h”
+#include "shell.h"
 
 /**
  **_strncpy - copies a string
-  *Return: the concatenated string
+ *@dest: the destination string to be copied to
+ *Return: the concatenated string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
 	char *s = dest;
-	
+
 	i = 0;
-	while (src[i] != ‘\0’ && i < n - 1)
+	while (src[i] != '\0' && i < n - 1)
 	{
 		dest[i] = src[i];
 		i++;
@@ -20,7 +21,7 @@ char *_strncpy(char *dest, char *src, int n)
 		j = i;
 		while (j < n)
 		{
-			dest[j] = ‘\0’;
+			dest[j] = '\0';
 			j++;
 		}
 	}
@@ -29,37 +30,39 @@ char *_strncpy(char *dest, char *src, int n)
 
 /**
  **_strncat - concatenates two strings
-  *Return: the concatenated string
+ *@dest: the first string
+ *Return: the concatenated string
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 	char *s = dest;
-	
+
 	i = 0;
 	j = 0;
-	while (dest[i] != ‘\0’)
+	while (dest[i] != '\0')
 		i++;
-	while (src[j] != ‘\0’ && j < n)
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
 	if (j < n)
-		dest[i] = ‘\0’;
+		dest[i] = '\0';
 	return (s);
 }
 
 /**
  **_strchr - locates a character in a string
-  *Return: (s) a pointer to the memory area s
+ *@s: the string to be parsed
  */
 char *_strchr(char *s, char c)
 {
 	do {
 		if (*s == c)
 			return (s);
-	} while (*s++ != ‘\0’);
+	} while (*s++ != '\0');
+
 	return (NULL);
 }
